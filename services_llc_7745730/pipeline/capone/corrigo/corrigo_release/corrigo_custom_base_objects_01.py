41a4eab8405844be246c9c9281a8c42317db9a8b
Varun-JLL <varun.kancharla@jll.com>
2023-08-02 10:26:55 -0500

    Update corrigo_custom_base_objects_01.py
    Adding score in employee

--- services_llc_7745730/pipeline/capone/corrigo/corrigo_release/corrigo_custom_base_objects_01.py
+++ services_llc_7745730/pipeline/capone/corrigo/corrigo_release/corrigo_custom_base_objects_01.py
@@ -213,6 +213,7 @@ df_employees = spark.sql(""" SELECT DISTINCT
                                             then 1
                                         else 0
                                     end as facility_manager_flag
+                                    ,r_employee.score as Score
                                 FROM {var_client_custom_db}.raw_employees_employees_corrigo          r_employee
                                 JOIN {var_client_custom_db}.custom_hv_master_clients_tenants         h_masterClientsTen
                                   ON TRIM(r_employee.source_id) = TRIM(h_masterClientsTen.source_id)
